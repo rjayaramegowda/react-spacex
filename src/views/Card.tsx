37c3cf500e0109c16970ed562d8962733f1afd15
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2021-12-18 13:56:52 +0530

    Update Card.tsx

--- src/views/Card.tsx
+++ src/views/Card.tsx
@@ -5,24 +5,59 @@ const Card = (props: VO) => {
   return (
     <div className="card border-0">
       <div>
-        <img
-          src={String(props.pic)}
-          className="img-fluid p-3"
-          alt="..."
-          style={{ minHeight: 235, minWidth: 235 }}
-        />
+        <a href={props.url} rel="noreferrer" target="_blank">
+          <img
+            src={String(props.pic)}
+            className="img-fluid p-3"
+            alt="..."
+            style={{ minHeight: 105, minWidth: 105 }}
+          />
+        </a>
       </div>
       <div className="card-body">
-        <p className="card-text text-center">{props.rocketName}</p>
-        <p className="card-text my-0">
+        <h5 className="card-text text-center">{props.rocketName}</h5>
+        <p className={"card-text text-secondary my-0"}>
           {new Date(props.launchDate).toDateString()}
         </p>
-        <p className="card-text my-0">
-          Status: {props.launchStatus ? "Success" : "Failure"}
-        </p>
-        <p className="card-text my-0">
-          Upcoming: {props.isUpcoming ? "Yes" : "No"}
-        </p>
+
+        <div className="d-flex justify-content-between">
+          <p
+            className={
+              props.launchStatus
+                ? "card-text text-success my-0"
+                : "card-text text-danger my-0"
+            }
+          >
+            {props.launchStatus ? "Success" : "Failure"}
+          </p>
+          <p
+            className={
+              props.isUpcoming
+                ? "card-text text-success my-0"
+                : "card-text text-secondary my-0"
+            }
+          >
+            Upcoming: {props.isUpcoming ? "Yes" : "No"}
+          </p>
+        </div>
+        <div className="d-flex justify-content-between pt-2 pb-4">
+          <a
+            href={props.youtube}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-outline-danger btn-sm"
+          >
+            Youtube
+          </a>
+          <a
+            href={props.url}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-outline-secondary btn-sm"
+          >
+            Readmore
+          </a>
+        </div>
       </div>
     </div>
   );
